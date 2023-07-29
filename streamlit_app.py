7e9d363271d6c1cb29d97abbd7d8b591fdaead0f
mkhan4ai <110739765+mkhan4ai@users.noreply.github.com>
2023-07-30 00:05:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,18 @@
+import streamlit as st
+
+def main():
+    st.title("Text Box and Select Option")
+
+    # Text box
+    user_input = st.text_input("Enter some text:", "")
+
+    # Select option
+    option = st.selectbox("Select an option:", ["Option 1", "Option 2", "Option 3"])
+
+    # Display the user input and selected option
+    st.write("You entered:", user_input)
+    st.write("You selected:", option)
+
+if __name__ == "__main__":
+    main()
 
